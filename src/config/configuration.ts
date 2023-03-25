7e55802642a42e813d928735e2acd8a1fb4289b5
naftali rayniz <naftali@wikey.ai>
2023-03-25 23:19:28 +0300

    .env JWT_EXPIRATION_TIME

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,4 +2,5 @@ export default () => ({
     PORT: parseInt(process.env.PORT, 10) || 3000,
     ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
     REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+    JWT_EXPIRATION_TIME: process.env.JWT_EXPIRATION_TIME,
 });
